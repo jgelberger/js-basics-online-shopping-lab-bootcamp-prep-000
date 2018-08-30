176f90cbf0fefbee5c767d3dee643cf25862b9ae
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-30 16:59:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,9 +77,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var card = cardNumber;
+  var total = total();
   if (card) {
     cart = [];
-    return `Your total cost is $${total()}, which will be charged to the card ${card}.`
+    return `Your total cost is $${total}, which will be charged to the card ${card}.`
   } else {
     return 'Sorry, we don\'t have a credit card on file for you.';
   }
