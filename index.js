0b48d05750429dc19d8239d69524915469fbe2a4
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-30 16:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var card = cardNumber;
-  total = total();
+  let total = total();
   if (card) {
     cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${card}.`
